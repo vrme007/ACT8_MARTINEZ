72a29018040cd5df7f9f955b304161b4d836e22e
vrme007 <martinezninojofel002@gmail.com>
2024-05-01 21:13:53 -0700

    Act8_Martinez

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+        <!-- Styles -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,31 @@
 @extends('layouts.app')
 @section('content')
-
 <main class="container">
     <section>
         <div class="titlebar">
             <h1>Products</h1>
-            <a href="{{route('products.create')}}" class="btn-link">Add Product</a>
+            <a href="{{route('products.create')}}" class='btn-link'>Add Product</a>
         </div>
+        @if ($message = Session::get('success'))
+            <script type="text/javascript">
+                const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+                });
+                Toast.fire({
+                icon: "success",
+                title: "Signed in successfully"
+                });
+            </script>
+        @endif
+
         <div class="table">
             <div class="table-filter">
                 <div>
@@ -17,19 +36,21 @@
                     </ul>
                 </div>
             </div>
-            <div class="table-search">
-                <div>
-                    <button class="search-select">
-                       Search Product
-                    </button>
-                    <span class="search-select-arrow">
-                        <i class="fas fa-caret-down"></i>
-                    </span>
+            <form method="GET" action="{{route('products.index')}}" accept-charset="UTF-8" role="search">
+                <div class="table-search">
+                    <div>
+                        <button class="search-select">
+                        Search Product
+                        </button>
+                        <span class="search-select-arrow">
+                            <i class="fas fa-caret-down"></i>
+                        </span>
+                    </div>
+                    <div class="relative">
+                        <input class="search-input" type="text" name="search" placeholder="Search product..." name="search" value="{{ request('search') }}">
+                    </div>
                 </div>
-                <div class="relative">
-                    <input class="search-input" type="text" name="search" placeholder="Search product..." value="{{ request('search') }}">
-                </div>
-            </div>
+            </form>
             <div class="table-product-head">
                 <p>Image</p>
                 <p>Name</p>
@@ -38,30 +59,30 @@
                 <p>Actions</p>
             </div>
             <div class="table-product-body">
-                <img src="1.jpg"/>
-                <p> Product name</p>
-                <p>Category</p>
-                <p>Inventory</p>
-                <div>
-                    <button class="btn btn-success" >
-                        <i class="fas fa-pencil-alt" ></i>
-                    </button>
-                    <button class="btn btn-danger" >
-                        <i class="far fa-trash-alt"></i>
-                    </button>
-                </div>
+                @if (count($products) > 0)
+                    @foreach ($products as $product )
+                        <img src="{{ asset('images/' . $product->image)}}"/>
+                        <p> {{$product->name}}</p>
+                        <p>{{$product->category}}</p>
+                        <p>{{$product->quantity}}</p>
+                        <div>
+                            <button class="btn btn-success" >
+                                <i class="fas fa-pencil-alt" ></i>
+                            </button>
+                            <button class="btn btn-danger" >
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    @endforeach
+                @else
+                        <p>Product not Found</p>
+                @endif
+
             </div>
             <div class="table-paginate">
-                <div class="pagination">
-                    <a href="#" disabled>&laquo;</a>
-                    <a class="active-page">1</a>
-                    <a>2</a>
-                    <a>3</a>
-                    <a href="#">&raquo;</a>
-                </div>
+                {{$products->links('layouts.pagination')}}
             </div>
         </div>
     </section>
 </main>
-
 @endsection
