6a4cb9196473ee707dfe454dc9b87fc817811927
vrme007 <martinezninojofel002@gmail.com>
2024-05-01 20:43:18 -0700

    Initial Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = "Product list form in ProductController";
+        return view('products.index', ['products' => $products]);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+}
